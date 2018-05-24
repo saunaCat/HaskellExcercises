1b6ff9bd58b5ce28c674cc704f84f804e7ae352d
Helena Strandberg <hejhej.helena@gmail.com>
2018-05-24 21:32:19 +0200

    Handle invalid input by returning error message. Calculate cents correctly. Still need to implement conversion from int to string name of int.

--- Data61ExcerciseHelenaStrandberg.hs
+++ Data61ExcerciseHelenaStrandberg.hs
@@ -8,28 +8,55 @@ removeMultiples xs = filter findMultiples xs
 
 findMultiples:: Int -> Bool
 findMultiples x 
-  | mod x 35 == 0 = True
-  | mod x 7 == 0  = False
-  | mod x 5 == 0  = False
-  | otherwise     = True
+  | mod x 35 == 0  = True
+  | mod x 7  == 0  = False
+  | mod x 5  == 0  = False
+  | otherwise      = True
       
 -- problem 2
 
 dollars:: String -> String
 dollars "" = "zero dollars and zero cents"
-dollars s  = do
-               let (dollars, cents) = splitDollarsAndCents s
-               concat [numToText dollars,  " dollars and ", numToText cents, " cents"]
+dollars s  
+  | isStringValid s == False = "Invalid string. You must input an int, float or double!"
+  | otherwise                = concat [numToText dollars, " dollars and ", 
+                                       numToText (cents), " cents"]
+  where (dollars, cents) = splitIntoDollarsAndCents s
                
+isStringValid:: String -> Bool
+isStringValid "" = True
+isStringValid s 
+  | all isDigit [ x | x <- s, not (x `elem` ".") ] == False = False
+  | otherwise                      = True
 
-splitDollarsAndCents:: String -> (Int, Int)
-splitDollarsAndCents "" = (0, 0)
-splitDollarsAndCents s = do 
-                           let [dollars, cents] = splitOn "." s
-                           (read dollars:: Int, read cents:: Int)
+
+splitIntoDollarsAndCents:: String -> (Int, Int)
+splitIntoDollarsAndCents "" = (0, 0)
+splitIntoDollarsAndCents s = do 
+                               let [wholeDollars, cents] = splitIntoList s
+                               textToNum [wholeDollars, (concat ["0.",cents])]
+                                
+splitIntoList:: String -> [String]
+splitIntoList "" = ["", ""]
+splitIntoList s 
+  | length xs == 1 = [ head xs , ""]
+  | otherwise      = xs
+    where xs = splitOn "." s
+    
+
+textToNum:: [String] -> (Int, Int)
+textToNum [] = (0, 0)
+textToNum [wholeDollars, cents] =
+  (read  $ zeroIfEmpty wholeDollars:: Int, 
+   round $ (read $ zeroIfEmpty cents:: Double)*100)
+                           
+zeroIfEmpty:: String -> String
+zeroIfEmpty "" = "0"
+zeroIfEmpty s = s
 
 numToText:: Int -> String
-numToText num = case num of 1  -> "one"
+numToText num = case num of 0 -> "zero"
+                            1 -> "one"
                             2  -> "two"
                             3  -> "three"
                             4  -> "four"
@@ -39,7 +66,14 @@ numToText num = case num of 1  -> "one"
                             8  -> "eight"
                             9  -> "nine"
                             10 -> "ten"
-                            -- reduce ....
+                            11 -> "eleven"
+                            20 -> "twenty"
+                            -- reduce ....?
+                            
+
+-- map first number and size of the rest to convert int into number word.... iterate..
+numeral:: (Int, Int) -> String
+numeral (firstNumber, sizeOfEntireNumber) = ""
 
 
 
